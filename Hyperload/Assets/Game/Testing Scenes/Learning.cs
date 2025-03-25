cdf17526223d8d1fb0f5d505475105bdc18ad941
maheshmajnu <mb8460777@gmail.com>
2025-03-25 19:09:13 +0530

    New changes

---
+++ Hyperload/Assets/Game/Testing Scenes/Learning.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learning : MonoBehaviour
+{
+
+    public string Playername = "Majnu";
+    public int Playerhealth = 100;
+    public bool IsAlive = true;
+    public float PlayerSpeed = 1.5f;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Printstats();
+        
+    }
+
+   void Printstats()
+
+    {
+        Debug.Log("Player Name :" + Playername);
+        Debug.Log("Player Health :" + Playerhealth);
+        Debug.Log("Player Alive :" + IsAlive);
+        Debug.Log("Player Speed :" + PlayerSpeed);
+    }
+
+
+
+
+}
